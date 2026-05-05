16f51feef23f0a1fecbc1a71492503197351b747
exclent <ekor4m4d4n1@gmail.com>
2026-05-05 20:30:06 +0700

    update(config) : add linter config typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,42 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
 import prettier from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'build']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     plugins: {
       prettier,
     },
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       eslintConfigPrettier,
     ],
+
     languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       globals: globals.browser,
     },
+
     rules: {
       'prettier/prettier': 'warn',
     },
