ceaf72d4925cd7a6dc12e1451963ae8840fee698
exclent <ekor4m4d4n1@gmail.com>
2026-05-05 20:24:34 +0700

    initial(frontend) : setup frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules', 'build']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      prettier,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+]);
