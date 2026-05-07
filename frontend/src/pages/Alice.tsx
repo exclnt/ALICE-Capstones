fcee8a58c61271e3de8ebf266d36439021be0480
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-07 16:30:49 +0700

    Remove React package and related files from node_modules, including production and development builds, JSX runtimes, and package metadata.

---
+++ frontend/src/pages/Alice.tsx
@@ -0,0 +1,7 @@
+export default function Alice() {
+  return (
+    <main>
+      <h1>HELLO Alice</h1>
+    </main>
+  );
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-1 md:h-full md:bg-bg-main md:rounded-xl p-5">
+      <h1 className="text-text-main">HELLO Home</h1>
+    </main>
+  );
+}
