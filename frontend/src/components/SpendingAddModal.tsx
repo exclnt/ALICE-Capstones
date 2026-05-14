6df1ad6af055840267371443a4cd7eb408dfe93e
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-14 17:05:37 +0700

    Feature: Implement animated and responsive `SpendingAddModal`

--- frontend/src/components/SpendingAddModal.tsx
+++ frontend/src/components/SpendingAddModal.tsx
@@ -1,42 +1,72 @@
+import { useLocation } from 'react-router-dom';
 import CurrencyInput from './CurrencyInput.tsx';
-import useInput from './hooks/useInput.tsx';
+import useInput from './hooks/useInput.ts';
 import SelectionInput from './SelectionInput.tsx';
 import TextInput from './TextInput.tsx';
 
+import { motion, AnimatePresence } from 'motion/react';
+import { useEffect } from 'react';
+import { useMediaQuery } from './hooks/useMediaQuery.ts';
+
 interface AddModalProp {
-  toggleVisible: () => void;
+  closeModal: () => void;
   isVisible: boolean;
 }
 
-export default function AddModal({ toggleVisible, isVisible }: AddModalProp) {
+export default function AddModal({ closeModal, isVisible }: AddModalProp) {
   const [amount, setAmount] = useInput('');
   const [title, setTitle] = useInput('');
   const options = ['Makanan', 'Minuman', 'Orang'];
   const [option, setOption] = useInput(options[0]);
 
+  const location = useLocation();
+  const isDesktop = useMediaQuery('(min-width:768px)');
+
+  useEffect(() => {
+    closeModal();
+  }, [location.pathname, closeModal]);
+
   return (
-    <section
-      className={`spending-modal fixed inset-0 z-30 flex items-end pb-21 ${isVisible ? 'visible opacity-100' : 'invisible opacity-0'}`}
-    >
-      <div className="absolute inset-0  backdrop-blur-sm" onClick={toggleVisible}></div>
-      <div
-        className={`bg-white z-30  pb-20 w-full transition-all duration-500 flex items-center flex-col p-3 rounded-tl-4xl rounded-tr-4xl ring-1 ring-primary/20 ${isVisible ? 'translate-y-0' : 'transalte-y-150 pointer-events-none'}`}
-      >
-        <header>
-          <h2 className="font-bold text-red-400">Pengeluaran</h2>
-        </header>
-        <form className="w-full p-5">
-          <CurrencyInput label="Jumlah (Rp)" onValueChange={setAmount} value={amount} />
-          <TextInput label="Judul Catatan" value={title} onChange={setTitle} />
-          <SelectionInput value={option} label="Kategori" onChange={setOption} options={options} />
-          <button
-            type="submit"
-            className="bg-primary font-medium text-bg-main rounded-xl p-2 mt-15 mb-10 w-full"
+    <AnimatePresence>
+      {isVisible && (
+        <section className="spending-modal fixed inset-0 z-30 md:z-100 flex items-end md:items-center md:justify-center pb-21 md:pb-0 px-1">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="absolute inset-0 backdrop-blur-sm bg-black/10"
+            onClick={closeModal}
+          />
+
+          <motion.div
+            initial={isDesktop ? { opacity: 0, scale: 0.8 } : { opacity: 0, y: '100%' }}
+            animate={isDesktop ? { opacity: 1, scale: 1 } : { opacity: 1, y: 0 }}
+            exit={isDesktop ? { opacity: 0, scale: 0.8 } : { opacity: 0, y: '100%' }}
+            transition={{ duration: 0.3, ease: 'easeOut' }}
+            className="bg-bg-main z-30 pb-20 md:pb-0 md:rounded-2xl w-full md:max-w-150 flex items-center flex-col p-3 rounded-tl-4xl rounded-tr-4xl ring-1 ring-primary/20"
           >
-            Masukan Pengeluaran
-          </button>
-        </form>
-      </div>
-    </section>
+            <header>
+              <h2 className="font-bold text-red-400">Pengeluaran</h2>
+            </header>
+            <form className="w-full p-5">
+              <CurrencyInput label="Jumlah (Rp)" onValueChange={setAmount} value={amount} />
+              <TextInput label="Judul Catatan" value={title} onChange={setTitle} />
+              <SelectionInput
+                value={option}
+                label="Kategori"
+                onChange={setOption}
+                options={options}
+              />
+              <button
+                type="submit"
+                className="bg-primary font-medium text-bg-main rounded-xl p-2 mt-15 mb-10 w-full"
+              >
+                Masukan Pengeluaran
+              </button>
+            </form>
+          </motion.div>
+        </section>
+      )}
+    </AnimatePresence>
   );
 }
