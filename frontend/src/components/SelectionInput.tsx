46e84bd1d55172f031b802b3af9cc783d896a1fd
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-14 15:03:15 +0700

    Adds reusable SelectionInput component

---
+++ frontend/src/components/SelectionInput.tsx
@@ -0,0 +1,39 @@
+import InputShell from './InputShell';
+import { useState } from 'react';
+
+interface SelectionInputProp {
+  label: string;
+  value: string;
+  onChange: (val: string) => void;
+  className?: string;
+  options: Array<string>;
+}
+
+export default function SelectionInput({
+  label,
+  value,
+  onChange,
+  className,
+  options,
+}: SelectionInputProp) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <InputShell label={label} isFocused={isFocused} hasValue={value.length > 0}>
+      <select
+        onChange={(e) => onChange(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className={`w-full bg-transparent px-4 pt-2 pb-2 text-text-main rounded-xl outline-none ${className}`}
+      >
+        {options.map((value: string) => {
+          return (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          );
+        })}
+      </select>
+    </InputShell>
+  );
+}
