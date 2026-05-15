37e9b45485ac099182356dec9b6ede65bda3a795
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-15 13:41:21 +0700

    Creates reusable PercentageBar component

---
+++ frontend/src/components/PercentageBar.tsx
@@ -0,0 +1,28 @@
+interface PrecentageBarProps {
+  percentage: number;
+  styleDiv?: string;
+  styleBar?: string;
+  label: string;
+}
+
+export default function PercentageBar({
+  percentage,
+  label,
+  styleDiv = 'h-2.5',
+  styleBar = 'bg-blue-500',
+}: PrecentageBarProps) {
+  return (
+    <div
+      className={`w-full bg-gray-300 dark:bg-gray-700 rounded-full  overflow-hidden ${styleDiv}`}
+      role={label}
+      aria-valuenow={percentage}
+      aria-valuemin={0}
+      aria-valuemax={100}
+    >
+      <div
+        className={`w-full h-full rounded-full transition-all duration-500 ease-out  ${styleBar}`}
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  );
+}
