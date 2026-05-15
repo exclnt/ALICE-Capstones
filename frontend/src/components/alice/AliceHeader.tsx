d1f40258b71df306af51cd60022dbb1203f5db2d
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-15 13:41:22 +0700

    Adjusts Alice header for improved responsive design

--- frontend/src/components/alice/AliceHeader.tsx
+++ frontend/src/components/alice/AliceHeader.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react';
 
 export default function AliceHeader() {
   return (
-    <header className="fixed top-0 left-0 right-0 p-3 flex flex-row items-center justify-between ring-1 ring-primary">
+    <header className="fixed md:static top-0 md:rounded-xl left-0 right-0 p-3 flex flex-row items-center justify-between ring-1 ring-primary">
       <div>
         <h1 className="text-xl font-bold text-text-main">A.L.I.C.E</h1>
         <div className="flex justify-between">
@@ -10,7 +10,7 @@ export default function AliceHeader() {
           <h3 className="text-xs text-text-muted">AI ONLINE</h3>
         </div>
       </div>
-      <h2 className="text-xs font-medium text-orange-400 bg-orange-400/25 rounded-xl p-1 px-2">
+      <h2 className="text-xs font-medium text-orange-400 bg-orange-400/25 rounded-xl p-1 px-2 ring-1 ring-orange-400/60">
         IMPLUSIF TINGGI
       </h2>
     </header>
