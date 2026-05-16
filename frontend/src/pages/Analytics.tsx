ba9437a73a7767a8120a8be474a230c9c69006c5
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-16 17:28:18 +0700

    Integrates new components into Analytics page

--- frontend/src/pages/Analytics.tsx
+++ frontend/src/pages/Analytics.tsx
@@ -1,7 +1,23 @@
+import AnalyticsHeader from '../components/analytics/AnalyticsHeader';
+import AnalyticsCard from '../components/analytics/AnalyticsCard';
+import AnalyticsChart from '../components/analytics/AnalyticsChart';
+
 export default function Analytics() {
   return (
-    <main>
-      <h1>HELLO Analytics</h1>
-    </main>
+    <section className="grid grid-cols-1 lg:grid-cols-2 h-full md:gap-4 bg-green-200">
+      {/* LEFT COLUMN: Chart dictates the height */}
+      <div className="flex flex-col px-5 pt-5 bg-green-200 w-full rounded-lg">
+        <AnalyticsHeader />
+        <div className="flex-1 min-h-0 w-full relative overflow-hidden pt-5">
+          <AnalyticsChart />
+        </div>
+      </div>
+
+      <div className="w-full h-105 flex flex-col lg:pt-16 md:h-full overflow-hidden -mt-1.5">
+        <div className="flex-1 w-full md:h-full bg-bg-main rounded-tl-2xl rounded-tr-2xl border-t-2 border-primary/50 pt overflow-hidden flex flex-col">
+          <AnalyticsCard />
+        </div>
+      </div>
+    </section>
   );
 }
