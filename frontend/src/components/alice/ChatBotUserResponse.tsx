a386e44e638891f99def5fd2f16c82de10bf54ee
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-27 14:28:11 +0700

    Adds core UI components for Alice chatbot

---
+++ frontend/src/components/alice/ChatBotUserResponse.tsx
@@ -0,0 +1,10 @@
+export default function ChatBotUserResponse() {
+  return (
+    <div className="bg-bg-main ring-1 rounded-tl-xl rounded-br-xl rounded-bl-xl py-2 px-5  ring-primary  h-fit w-fit flex items-start justify-start flex-col gap-3">
+      <p className="text-text-main ">
+        Halo! Saya adalah asisten virtual prototype. Ada yang bisa saya bantu hari ini?
+      </p>
+      <p className="text-text-muted">13.57</p>
+    </div>
+  );
+}
